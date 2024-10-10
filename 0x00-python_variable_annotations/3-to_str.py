29073163ecaecb06a76c3310b0ec6b58a839e28d
Ebnen <Ebenzy1996@gmail.com>
2024-10-10 15:02:34 +0100

    python-continue

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type-annotated function to_str that takes a float n as argument"""
+
+
+def to_str(n: float) -> str:
+    """return a float as a string"""
+    return str(n)
