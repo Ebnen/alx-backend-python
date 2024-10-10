7193d1aee61f0ca880110797dd53aec0b55071c3
Ebnen <Ebenzy1996@gmail.com>
2024-10-10 13:50:02 +0100

    python-continue

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""type-annotated function add that takes a  and returns their sum as a float"""
+"""type-annotated func a  and returns their sum as a float"""
+
 
 def add(a: float, b: float) -> float:
     """return sum of two floats"""
