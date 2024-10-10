e2e32332e7cae2dca21dce79b70cf707f5398596
Ebnen <Ebenzy1996@gmail.com>
2024-10-10 17:29:30 +0100

    python-continue

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ function element_length that takes a list l of strings as argument """
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """return a list of tuples"""
     return [(i, len(i)) for i in lst]
