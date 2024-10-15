ca286b0edc4c5abae4de51cf3c9239cc33b58a23
Ebnen <Ebenzy1996@gmail.com>
2024-10-15 18:12:04 +0100

    async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""trying out the magic word"""
+"""asynchronous coroutine that takes in an integer args"""
+
 import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
-    """a funtio that return delay"""
-    delays = [] 
-    for _ in range(n):
-        delay = await wait_random(max_delay)
-        i = 0
-        while i < len(delays) and delays[i] < delay:
-            i += 1
-        delays.insert(i, delay)
-    return delays
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+     Async routine that spawns wait_random n  max_delay.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
